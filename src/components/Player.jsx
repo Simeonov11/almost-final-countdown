64d4bbed6f61fa5de1756ad049b1faefd62b13f4
Simo <simeonov.pa@gmail.com>
2025-04-19 23:06:49 +0100

    set player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
 
 export default function Player() {
+    const [enteredPlayerName, setEnteredPlayerName] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleChange = (event) => {
+        setSubmitted(false);
+        setEnteredPlayerName(event.target.value);
+    }
+
+    const handleClick = () => {
+        setSubmitted(true);
+    }
+
     return (
         <section id="player">
             <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'} </h2>
