4298b861aa9b2ca8728c7a4f1bd090cb0ea92bf7
Simo <simeonov.pa@gmail.com>
2025-04-19 23:38:10 +0100

    clear the input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -8,6 +8,7 @@ export default function Player() {
 
     const handleClick = () => {
         setEnteredPlayerName(playerName.current.value);
+        playerName.current.value = '';
     }
 
     return (
